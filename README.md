aab3f150679ec3f54d732d0a323c46cc8eb4f445
sauk1346 <atmoralesc@gmail.com>
2024-04-08 15:20:20 -0400

    se crea arquitectura de carpetas

---
+++ README.md
@@ -0,0 +1,10 @@
+**\[UDD] Desarrollo Web FullStack C13**
+
+# Proyecto 02 : Sistema de Votación en JS
+
+## 1. Introducción
+
+## 2. Desarrollo
+
+## 3. Conclusión
+
---
+++ pf/index.js
@@ -0,0 +1,10 @@
+/*
+--- PF solution ---
+Construir un programa en JavaScript que permita a los usuarios crear encuestas, votar y ver los resultados en tiempo real.
+- Permitir a los usuarios crear encuestar con opciones de respuesta.
+- Permitir a los usuarios votar en las encuestas.
+- Mostrar los resultados de las encuestas.
+- Almacenar los datos de las encuestas y los votos en una variable.
+- Almacenar los datos de las encuestas y los votos en una estructura de datos.
+- Las encuestas deben contener al menos 8 preguntas con opciones de respuesta.
+*/
